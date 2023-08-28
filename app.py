febd0d37d6c1031e7e9182a28ab874ffe3f8945c
sanketchaudhary10 <67291486+sanketchaudhary10@users.noreply.github.com>
2023-08-27 23:08:51 -0400

    Adding Files

--- app.py
+++ app.py
@@ -5,21 +5,18 @@ import os
 
 app = Flask(__name__)
 
-# Set up OpenAI API credentials
 openai.api_key = "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"
 
 
-# Define the default route to return the index.html file
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# Define the /api route to handle POST requests
 @app.route("/api", methods=["POST"])
 def api():
-    # Get the message from the POST request
+    # Getting message from the POST request
     message = request.json.get("message")
-    # Send the message to OpenAI's API and receive the response
+    # Sending message to OpenAI's API and receiving response
     
     
     responses = openai.ChatCompletion.create(
